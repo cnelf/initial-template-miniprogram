eae3db9d6b0cc782a60c2c361ccb2267ea49a8e1
cnelf <1184974829@qq.com>
2022-05-26 11:32:38 +0800

    fix: 代码格式调整

--- src/app.js
+++ src/app.js
@@ -1,16 +1,15 @@
 import '@/lib/polyfill';
 import { promisifyAll } from '@/lib/wx-promise-pro';
-import { history, relaunchCurrentPage } from '@/utils/index';
+import { history } from '@/utils/index';
 import { store } from '@/store/index';
 import env from '@/api/env';
 
 App({
-  globalData: {}, // 请使用'/utils/store.js'替代globalData
+  globalData: {}, // 请使用'/utils/store'替代globalData
   async onLaunch() {
     promisifyAll();
     wx.$env = env;
     wx.$router = history;
-    return relaunchCurrentPage();
   },
   store
 });
